7a42e9c024857c48718922ef15ed8a6283d316ad
AriaieBOY <smr.seddighy@gmail.com>
2022-12-02 02:30:55 +0330

    add contact page

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -5,66 +5,35 @@ description: Get in touch with us
 @extends('_layouts.main')
 
 @section('body')
-<h1>Contact</h1>
-
-<p class="mb-8">
-    Static sites are unable to handle form submissions. However, there are third-party services, like Tighten’s <a href="https://fieldgoal.io" title="FieldGoal">FieldGoal</a>, which can accept the form submission, email you the result, and redirect back to a thank you page.
-</p>
-
-<form action="/contact" class="mb-12">
-    <div class="flex flex-wrap mb-6 -mx-3">
-        <div class="w-full md:w-1/2 mb-6 md:mb-0 px-3">
-            <label class="block mb-2 text-gray-800 text-sm font-semibold" for="contact-name">
-                Name
-            </label>
-
-            <input
-                type="text"
-                id="contact-name"
-                placeholder="Jane Doe"
-                name="name"
-                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                required
-            >
+    <div class="py-16 lg:py-20 prose dark:prose-dark">
+        <div>
+            <img src="/assets/img/icon-contact.png" alt="icon envelope"/>
         </div>
-
-        <div class="w-full px-3 md:w-1/2">
-            <label class="block text-gray-800 text-sm font-semibold mb-2" for="contact-email">
-                Email Address
-            </label>
-
-            <input
-                type="email"
-                id="contact-email"
-                placeholder="email@domain.com"
-                name="email"
-                class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
-                required
-            >
-        </div>
-    </div>
-
-    <div class="w-full mb-12">
-        <label class="block text-gray-800 text-sm font-semibold mb-2" for="contact-message">
-            Message
-        </label>
-
-        <textarea
-            id="contact-message"
-            rows="4"
-            name="message"
-            class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-3"
-            placeholder="A lovely message here."
-            required
-        ></textarea>
-    </div>
-
-    <div class="flex justify-end w-full">
-        <input
-            type="submit"
-            value="Submit"
-            class="block bg-blue-500 hover:bg-blue-600 text-white text-sm font-semibold leading-snug tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
+        <h1
+                class="pt-5 font-body text-4xl font-semibold text-primary dark:text-white md:text-5xl lg:text-6xl"
         >
+            Contact
+        </h1>
+        <div class="pr-2 pt-3 sm:pt-0">
+            <p class="font-body text-xl font-light text-primary dark:text-white">
+                Got a question or proposal, or just want to say hello? Go ahead. You know what to do.
+            </p>
+        </div>
+
+        <div class="flex gap-2">
+            <a href="https://twitter.com/smr_seddighy" target="blank"><img align="center"
+                                                                           src="https://img.shields.io/static/v1?style=for-the-badge&message=Twitter&color=1DA1F2&logo=Twitter&logoColor=FFFFFF&label="
+                                                                           alt="smr_seddighy"/></a>
+            <a href="https://instagram.com/ariaieboy" target="blank"><img align="center"
+                                                                          src="https://img.shields.io/static/v1?style=for-the-badge&message=Instagram&color=E4405F&logo=Instagram&logoColor=FFFFFF&label="
+                                                                          alt="ariaieboy"/></a>
+            <a href="https://t.me/ariaieboy" target="blank"><img align="center"
+                                                                 src="https://img.shields.io/static/v1?style=for-the-badge&message=Telegram&color=26A5E4&logo=Telegram&logoColor=FFFFFF&label="
+                                                                 alt="ariaieboy"/></a>
+            <a href="https://wa.me/989158585889?text=hi,%20i%20am%20contacting%20from%20github" target="blank"><img
+                        align="center"
+                        src="https://img.shields.io/static/v1?style=for-the-badge&message=WhatsApp&color=222222&logo=WhatsApp&logoColor=25D366&label="
+                        alt="+989158585889"/></a>
+        </div>
     </div>
-</form>
 @stop
