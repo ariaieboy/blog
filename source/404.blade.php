6933df516614d50aed3f3fdfcbdb73ddbfd19c81
AriaieBOY <smr.seddighy@gmail.com>
2022-12-02 01:22:46 +0330

    add torchlight

--- source/404.blade.php
+++ source/404.blade.php
@@ -5,7 +5,6 @@
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
 
         <h2 class="text-3xl">Page not found.</h2>
-
         <hr class="block w-full max-w-sm mx-auto border my-8">
     </div>
 @endsection
