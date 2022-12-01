2cf80c13fa7cb750366a6b3030f6402eaf5e0ff3
AriaieBOY <smr.seddighy@gmail.com>
2022-12-02 02:49:27 +0330

    add instagram link

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -225,6 +225,11 @@
                         class="text-4xl text-primary dark:text-white pl-5 hover:text-secondary dark:hover:text-secondary transition-colors bx bxl-twitter"
                     ></i>
                 </a>
+                <a href="https://instagram.com/ariaieboy" target="_blank">
+                    <i
+                        class="text-4xl text-primary dark:text-white pl-5 hover:text-secondary dark:hover:text-secondary transition-colors bx bxl-instagram"
+                    ></i>
+                </a>
                 <a href="https://t.me/ariaieboy" target="_blank">
                     <i
                         class="text-4xl text-primary dark:text-white pl-5 hover:text-secondary dark:hover:text-secondary transition-colors bx bxl-telegram"
