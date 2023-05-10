6a763f8c99831f2dcffc79d000b3c92795ec6b01
AriaieBOY <ariaieboy.ir@gmail.com>
2023-05-10 13:32:16 +0330

    fix category layout

--- source/_layouts/category.blade.php
+++ source/_layouts/category.blade.php
@@ -1,13 +1,14 @@
 @extends('_layouts.main')
 
 @section('body')
-    <div class="py-16 lg:py-20">
-        <h1 class="pt-5 font-body text-4xl font-semibold text-primary dark:text-white md:text-5xl lg:text-6xl">{{ $page->title }}</h1>
+    <div class="py-4 lg:py-8">
+        <h1 class="pt-5 font-body text-4xl font-semibold md:text-5xl lg:text-6xl">{{ $page->title }}</h1>
 
-        <div class="font-body text-xl font-light text-primary dark:text-white">
+        <div class="text-xl font-light">
             @yield('content')
         </div>
-        <div class="pt-8 lg:pt-12">
+
+        <div class="pt-4 lg:pt-8 flex flex-col gap-4">
             @foreach ($page->posts($posts) as $post)
                 @include('_components.post-preview-inline')
             @endforeach
