39fd1840b78d7f550f720fae522d0b21291415d6
AriaieBOY <ariaieboy.ir@gmail.com>
2026-06-29 18:40:55 +0330

    remove torchlight

--- bootstrap.php
+++ bootstrap.php
@@ -18,4 +18,3 @@ $events->afterBuild(App\Listeners\GenerateSitemap::class);
 $events->afterBuild(App\Listeners\GenerateIndex::class);
 $events->afterCollections(App\Listeners\GenerateEstimateReadingTime::class);
 
-\Torchlight\Jigsaw\TorchlightExtension::make($container, $events)->boot();
